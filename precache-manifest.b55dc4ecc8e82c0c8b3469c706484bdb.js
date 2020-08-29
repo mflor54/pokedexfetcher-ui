89a88f23b40553fb2b225c015595f9eed6685465
Michael <mflor54@gmail.com>
2020-08-29 17:16:52 -0400

    Updates

---
+++ precache-manifest.b55dc4ecc8e82c0c8b3469c706484bdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f9985a2a464d82c1649dbb403446e8",
+    "url": "/pokedexfetcher-ui/index.html"
+  },
+  {
+    "revision": "dc13d93f04a64322849a",
+    "url": "/pokedexfetcher-ui/static/css/main.c0c2db1b.chunk.css"
+  },
+  {
+    "revision": "a291d5ac62e069dba6c4",
+    "url": "/pokedexfetcher-ui/static/js/2.40f0e1b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedexfetcher-ui/static/js/2.40f0e1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc13d93f04a64322849a",
+    "url": "/pokedexfetcher-ui/static/js/main.bec9fbba.chunk.js"
+  },
+  {
+    "revision": "7dc2e4e61a33ae82f7a6",
+    "url": "/pokedexfetcher-ui/static/js/runtime-main.53a1cdf3.js"
+  }
+]);
